552c6d6ec55ca952415750e9c6bd62212cf4d3a0
Roman <s-3m@yandex.ru>
2022-09-08 15:43:15 +0300

    hw 2 done

--- framework/main.py
+++ framework/main.py
@@ -1,3 +1,7 @@
+from quopri import decodestring
+from framework.type_requests import PostRequest, GetRequest
+
+
 class PageNotFound:
     def __call__(self, *args, **kwargs):
         return '404 WHAT', '404 page not found'
@@ -10,6 +14,7 @@ class BananaFramework:
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
+        # pprint(environ)
 
         if not path.endswith('/'):
             path = path + '/'
@@ -21,6 +26,18 @@ class BananaFramework:
 
         request = {}
 
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequest().get_request_param(environ)
+            request['data'] = BananaFramework.decode_data(data)
+            print(f'Input POST request with params: {request["data"]}')
+        if method == 'GET':
+            data = GetRequest().get_request_param(environ)
+            request['request_param'] = BananaFramework.decode_data(data)
+            print(f'Input GET request with params: {request["request_param"]}')
+
         if self.fronts is not None:
             for front in self.fronts:
                 front(request)
@@ -28,3 +45,12 @@ class BananaFramework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_data(data: dict):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace('+', ' '), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
---
+++ framework/type_requests.py
@@ -0,0 +1,52 @@
+class GetRequest:
+
+    @staticmethod
+    def pars_params(params: str):
+        result = {}
+        if params:
+            data = params.split('&')
+            for item in data:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    @staticmethod
+    def get_request_param(environ):
+        query_string: str = environ['QUERY_STRING']
+        query_params = GetRequest.pars_params(query_string)
+        return query_params
+
+
+class PostRequest:
+
+    @staticmethod
+    def pars_params(params: str):
+        result = {}
+        if params:
+            data = params.split('&')
+            for item in data:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    def get_wsgi_data(env) -> bytes:
+        content_len_data = env.get('CONTENT_LENGTH')
+        content_len = int(content_len_data) if content_len_data else 0
+        print(content_len)
+
+        data = env['wsgi.input'].read(content_len) if content_len > 0 else b''
+        return data
+
+    def pars_wsgi_data(self, data: bytes) -> dict:
+        result = {}
+        if data:
+            data_str = data.decode(encoding='utf-8')
+            print(f'String after decoding - {data_str}')
+
+            result = PostRequest.pars_params(data_str)
+        return result
+
+    def get_request_param(self, environ):
+        data = PostRequest.get_wsgi_data(environ)
+        data = self.pars_wsgi_data(data)
+        return data
