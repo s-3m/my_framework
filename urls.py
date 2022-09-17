b64a1c3d5fe50760df56c6e53979e426ea7285d8
Roman <s-3m@yandex.ru>
2022-09-17 16:19:39 +0300

    add feature

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
-from views import Index, About, Contact, Catalog
+from views import Index, About, Contact, Catalog, CreateGenre, CreateFilm
 
 routes = {
     '/': Index(),
     '/about/': About(),
     '/contact/': Contact(),
-    '/catalog/': Catalog()
+    '/catalog/': Catalog(),
+    '/AddGenre/': CreateGenre(),
+    '/AddFilm/': CreateFilm()
 }
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from framework.templator import render
-from patterns.creational import Engine, Logger
+from patterns.creational import Engine, Logger, FilmFactory
 
 engine = Engine()
 logger = Logger('main')
@@ -7,7 +7,7 @@ logger = Logger('main')
 
 class Index:
     def __call__(self, request):
-        context = {'title': 'Главная'}
+        context = {'title': 'Главная', 'genre': engine.genre}
         return '200 OK', render('index.html', request=request, context=context)
 
     def __repr__(self):
@@ -16,7 +16,7 @@ class Index:
 
 class Contact:
     def __call__(self, request):
-        context = {'title': 'Контакты'}
+        context = {'title': 'Контакты', 'genre': engine.genre}
         return '200 OK', render('contact.html', request=request, context=context)
 
     def __repr__(self):
@@ -25,7 +25,7 @@ class Contact:
 
 class About:
     def __call__(self, request):
-        context = {'title': 'О проекте'}
+        context = {'title': 'О проекте', 'genre': engine.genre}
         return '200 OK', render('about.html', request=request, context=context)
 
     def __repr__(self):
@@ -43,3 +43,28 @@ class Catalog:
 
     def __repr__(self):
         return self.__class__.__name__
+
+
+class CreateGenre:
+    def __call__(self, request):
+        context = {
+            'title': 'Новый жанр',
+            'genre': engine.genre
+        }
+        if request['method'] == 'POST':
+            genre_name = request['data']['genre_name']
+            new_genre = engine.create_genre(genre_name)
+            engine.genre.append(new_genre)
+            context = {'genre': engine.genre, 'title': 'Главная'}
+            return '200 OK', render('index.html', context=context, request=request)
+        return '200 OK', render('create_genre.html', request=request, context=context)
+
+
+class CreateFilm:
+    def __call__(self, request):
+        context = {
+            'title': 'Новый жанр',
+            'genre': engine.genre,
+            'film_types': FilmFactory.types.keys()
+        }
+        return '200 OK', render('create_film.html', request=request, context=context)
